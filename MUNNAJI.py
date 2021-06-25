e3a12c52c18dc42661da7370df275a22782c9fbc
zayn011 <85694862+zayn011@users.noreply.github.com>
2021-06-25 22:36:42 +0530

    Update MUNNAJI.py

--- MUNNAJI.py
+++ MUNNAJI.py
@@ -2,7 +2,7 @@ from userbot import bot as MUNNAgalaxy
 from userbot.utils import admin_cmd as choot
 
 x = True
-@MUNNAgalaxy.on(choot(pattern="bsdk"))
+@MUNNAgalaxy.on(choot(pattern="zayn"))
 async def yashraid(event):
     while x != False:
        
@@ -15,3 +15,19 @@ async def yashraid(event):
     await a.edit("Y")
      await asyncio.sleep(0.9)
     await a.edit("N")
+    
+@MUNNAgalaxy.on(choot(pattern="anisha"))
+async def yashraid(event):
+    while x != False:
+        
+         a=await MUNNAgalaxy.send_message(kek, "A")
+        await asyncio.sleep(0.9)
+    await a.edit("N")
+     await asyncio.sleep(0.9)
+    await a.edit("I")
+     await asyncio.sleep(0.9)
+    await a.edit("S")
+     await asyncio.sleep(0.9)
+    await a.edit("H")
+     await asyncio.sleep(0.9)
+    await a.edit("A")
